81060f65a637c1ae37f263d79048d0f8f7e13f8a
Sheila-otuko <mercisheela3@gmail.com>
2022-07-05 05:13:22 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet in lower case
+*
+* Return: 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A folder for functions and nested loops
