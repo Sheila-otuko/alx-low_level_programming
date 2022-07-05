4a465bd164605cc3808cf3f675df31122442548b
Sheila-otuko <mercisheela3@gmail.com>
2022-07-05 06:00:52 -0700

    prints using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/*
-* main - Prints _putchar
-*
-*Return : Always 0 (Success)
+/**
+* main - Entry point
+* Function prints _putchar
+*Return: Always 0 (Success)
 */
 int main(void)
 {
