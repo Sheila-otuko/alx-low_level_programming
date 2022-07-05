26c6906fdfcbee3eeec007e94151ebe701a27a63
Sheila-otuko <mercisheela3@gmail.com>
2022-07-05 06:20:07 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Prints the alphabet 10 times
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count <= 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		count++;
+		_putchar('\n');
+	}
+}
