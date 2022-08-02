ece191cbee1394d5505090463f0518b27e94c4f0
Sheila-otuko <mercisheela3@gmail.com>
2022-08-02 11:51:43 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,83 +1,70 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: malloced dog_t
+ * _strdup - duplicate a string
+ * @str: string
+ *
+ * Return: pointer to newely allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	char *temp;
+	int i, j;
+
+	if (!str)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	temp = malloc(sizeof(char) * ++i);
+
+	if (!temp)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		temp[j] = str[j];
+
+	return (temp);
+}
+
+/**
+ * new_dog - structure
+ * @name: member
+ * @age: member
+ * @owner: member
+ *
+ * Return: dog_t
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *n, *o;
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *new_dog;
 
-	if (!new_dog || !name || !owner)
+	if (!name || !owner)
+		return (NULL);
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (!new_dog)
+		return (NULL);
+
+	new_dog->name = _strdup(name);
+	if (!new_dog->name)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-	n = malloc(_strlen(name) + 1);
-	if (!n)
-	{
-		return (free(new_dog), NULL);
-	}
-	n = _strdup(name);
-	new_dog->name = n;
-	o = malloc(_strlen(owner) + 1);
-	if (!o)
-	{
-		return (free(new_dog->name), free(new_dog), NULL);
-	}
-	o = _strdup(owner);
-	new_dog->owner = o;
+
 	new_dog->age = age;
+
+	new_dog->owner = _strdup(owner);
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
 	return (new_dog);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-	{
-		s++;
-	}
-	return (s - p);
-}
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
- * @str: string to be copied
- * Return: copied string
- */
-
-char *_strdup(char *str)
-{
-	int i, len;
-	char *copy;
-
-	if (!str)
-	{
-		return (NULL);
-	}
-	len = _strlen(str);
-	copy = malloc(sizeof(char) * len + 1);
-	if (!copy)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < len; i++)
-	{
-		copy[i] = str[i];
-	}
-	copy[i] = 0;
-	return (copy);
 }
